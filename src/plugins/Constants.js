0938cd425d4c9bcb266f12d6025085919c81629e
Eduardo Chueca <cm89eduardo@gmail.com>
2023-05-16 11:12:52 +0200

    Añadidas tablas

--- src/plugins/Constants.js
+++ src/plugins/Constants.js
@@ -3,42 +3,53 @@ export default {
     Vue.prototype.$constants = () => {
       return {
         VERSION: '1.0.0',
-        usuarioTableFields:[
+        usuarioTableFields: [
           { key: 'nombre', label: 'Nombre', sortable: true },
           { key: 'nif', label: 'DNI', sortable: true },
           { key: 'action', label: '', thStyle: { width: '120px'} }
         ],
-        fraseTableFields:[
+        fraseTableFields: [
           { key: 'codigo', label: 'Codigo', sortable: true },
           { key: 'descripcion', label: 'Descripcion'},
           { key: 'action', label : '', thStyle: { width: '120px'} }
         ],
-        armarioTableFields:[
+        armarioTableFields: [
           { key: 'nombre', label: 'Nombre', sortable: true },
           { key: 'descripcion', label: 'Descripcion', sortable: true },
           { key: 'action', label: '', thStyle: { width: '180px'} }
         ],
-        estanteTableFields:[
+        estanteTableFields: [
           { key: 'nombre', label: 'Nombre', sortable: true },
           { key: 'descripcion', label: 'Descripcion', sortable: true },
           { key: 'childAction', label: '', thStyle: { width: '120px'} }          
         ],
-        etiquetaTableFields:[
+        etiquetaTableFields: [
           { key: 'codigo', label: 'Código', sortable: true },
           { key: 'descripcion', label: 'Descripción', sortable: true },
           { key: 'image', label: 'Imagen', thStyle: { width: '100px'} },
           { key: 'action', label: '', thStyle: { width: '120px'} }
         ],
-        compuestoTableFields:[
+        compuestoTableFields: [
           { key: 'cas', label: 'Nº CAS', sortable: true, thStyle: { width: '120px'} },
           { key: 'nombre', label: 'Nombre', sortable: true },
           { key: 'image', label: 'Imagen', thStyle: { width: '100px'} },
           { key: 'action', label: '', thStyle: { width: '120px'} }
         ],
-        envaseTableFields:[
-          { key: 'codigo', label: 'Código' },
-          { key: ''}
-        ]
+        envaseTableFields: [
+          { key: 'codigo', label: 'Código envase', thStyle: { width: '140px'} },
+          { key: 'nombre', label: 'Nombre', sortable: true },
+          { key: 'pureza', label: 'Pureza', thStyle: { width: '140px'} },
+          { key: 'cantidad', label: 'Cantidad', thStyle: { width: '140px'} },
+          { key: 'image_inside', label: '', thStyle: { width: '100px' } },
+          { key: 'action', label: '', thStyle: { width: '120px' } }
+        ],
+        solicitudUsuarioTableFields: [
+          { key: 'fechaSolicitud', label: 'Fecha', sortable: true },
+          { key: 'estado', label: 'Estado', sortable: true },
+          { key: 'idRecipiente', label: 'recipiente' },
+          { key: 'descripcion', label: 'descripcion' },
+          { key: 'usuarioTramite', label: 'tramitado por' }
+        ],
       };
     };
   }
