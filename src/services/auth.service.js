23d30888be65c70c76d7f7f3a74fcfcfcdae8e88
Eduardo Chueca <cm89eduardo@gmail.com>
2023-06-06 17:44:58 +0200

    Métodos edicion usuarios

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -16,6 +16,61 @@ class AuthService {
       });
   }
 
+  get() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    return axios
+      .get('http://localhost:9098/api/auth/user', {
+        headers: {
+          Authorization: 'Bearer ' + user.accessToken
+        }
+      })
+      .then(response => response.data);
+  }
+
+  setPassword(data) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    return axios
+      .put('http://localhost:9098/api/auth/password', data, {
+        headers: {
+          Authorization: 'Bearer ' + user.accessToken
+        }
+      })
+      .then(response => response.data);
+  }
+
+  updateUser(data) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    return axios
+    .put('http://localhost:9098/api/auth/user', data, {
+      headers: {
+        Authorization: 'Bearer ' + user.accessToken
+      }
+    })
+    .then(response => response.data);
+  }
+
+  signup(data) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    return axios
+      .post('http://localhost:9098/api/auth/signup', data, {
+        headers: {
+          Authorization: 'Bearer ' + user.accessToken
+        } 
+      })
+      .then(response => response.data);
+  }
+
+  delete(id) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    return axios
+      .delete('http://localhost:9098/api/auth/user/' + id, {
+        headers: {
+          Authorization: 'Bearer ' + user.accessToken
+        } 
+      })
+      .then(response => response.data);
+  }
+
   logout() {
     localStorage.removeItem('user');
   }
